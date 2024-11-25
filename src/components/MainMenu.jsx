f8bd8b90c446418215741edd29f8af3e09852dda
unknown <riosh.valeria@gmail.com>
2024-11-25 15:32:08 -0500

    a few modifications

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -4,7 +4,7 @@ import './MainMenu.css'
 function MainMenu({ onStartGame, onViewScores }) {
   return (
     <div className="main-menu">
-      <h1>Box Destroyer</h1>
+      <h1>THE SHOTER</h1>
       <div className="menu-buttons">
         <button onClick={onStartGame}>Start Game</button>
         <button onClick={onViewScores}>View Scores</button>
